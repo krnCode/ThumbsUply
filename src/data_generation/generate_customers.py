3c0521a44318520f6fd0729f3ec0b6348a4fe4ac
krn <contadorkrn@gmail.com>
2025-12-27 12:45:03 -0300

    DEV: Desenvolver etapa de inclusão de dados em tabela do Supabase

--- src/data_generation/generate_customers.py
+++ src/data_generation/generate_customers.py
@@ -5,9 +5,20 @@ Arquivo para a geração de dados fictícios para clientes, utilizando a API do 
 import uuid
 import requests as r
 import polars as pl
+import os
 
-from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from datetime import datetime
 from src.utils.helpers import generate_random_date
+from src.utils.supabase_tools import create_supabase_client, update_supabase_table
+
+
+# region ----- Carregar variáveis de ambiente -----
+load_dotenv()
+
+SUPABASE_URL: str = os.getenv("SUPABASE_URL")
+SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
+# endregion
 
 
 # region ----- Funções -----
@@ -27,7 +38,7 @@ def create_customers_data(response) -> pl.DataFrame:
     results = response["results"]
     customers_data = pl.DataFrame(
         {
-            "id": pl.Series([str(uuid.uuid4()) for x in range(len(results))]),
+            "customer_id": pl.Series([str(uuid.uuid4()) for x in range(len(results))]),
             "created_at": pl.Series(
                 [generate_random_date() for x in range(len(results))]
             ),
@@ -35,12 +46,12 @@ def create_customers_data(response) -> pl.DataFrame:
             "last_name": pl.Series([x["name"]["last"] for x in results]),
             "gender": pl.Series([x["gender"] for x in results]),
             "email": pl.Series([x["email"] for x in results]),
-            "nat": pl.Series([x["nat"] for x in results]),
+            "nationationality": pl.Series([x["nat"] for x in results]),
             "location_country": pl.Series([x["location"]["country"] for x in results]),
             "location_state": pl.Series([x["location"]["state"] for x in results]),
             "location_city": pl.Series([x["location"]["city"] for x in results]),
-            "cell": pl.Series([x["cell"] for x in results]),
-            "picture": pl.Series([x["picture"]["large"] for x in results]),
+            "cellphone_number": pl.Series([x["cell"] for x in results]),
+            "customer_thumbnail": pl.Series([x["picture"]["large"] for x in results]),
         }
     )
 
@@ -66,5 +77,17 @@ response: dict = r.get(
 
 if __name__ == "__main__":
     customers_data = create_customers_data(response)
+    # Converter datas para strings para serem compatíveis com o supabase
+    customers_data = customers_data.with_columns(
+        pl.col("created_at").dt.strftime("%Y-%m-%dT%H:%M:%S")
+    )
+    # Transformar dataframe em dict para ser compatível com o supabase
+    customers_data_dict = customers_data.to_dicts()
 
-    print(customers_data)
+    supabase = create_supabase_client(url=SUPABASE_URL, key=SUPABASE_KEY)
+    update_supabase_table(
+        supabase_client=supabase,
+        table_name="customers",
+        data=customers_data_dict,
+    )
+    # print(customers_data)
