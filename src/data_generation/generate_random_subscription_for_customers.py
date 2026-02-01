5c49ac8f703a95ebe8b68fb5b4e2b4a2d8445276
krn <contadorkrn@gmail.com>
2026-02-01 12:15:38 -0300

    DEV: Desenvolver tabela de snapshot de eventos

--- src/data_generation/generate_random_subscription_for_customers.py
+++ src/data_generation/generate_random_subscription_for_customers.py
@@ -3,6 +3,8 @@ Arquivo para a geração inicial de assinaturas por cliente.
 """
 
 import os
+import uuid
+import numpy as np
 import polars as pl
 
 from dotenv import load_dotenv
@@ -14,6 +16,7 @@ load_dotenv()
 supabase_url = os.getenv("SUPABASE_URL")
 supabase_key = os.getenv("SUPABASE_KEY")
 # endregion
+
 
 if __name__ == "__main__":
     # region ----- Conexão com Supabase -----
@@ -34,5 +37,36 @@ if __name__ == "__main__":
     subscriptions_plans_df = pl.DataFrame(subscriptions_plans)
     # endregion
 
-    print(customers_df)
-    print(subscriptions_plans_df)
+    # region ----- Transformar Subs em Lista -----
+    subscriptions_plans_list: list = subscriptions_plans_df[
+        "subscription_plan_id"
+    ].to_list()
+    print(subscriptions_plans_list)
+    # endregion
+
+    # region ----- Selecionar Sub aleatório para Customers -----
+    subscription_weights: list[float] = [0.7, 0.2, 0.1]
+
+    customer_subscription: pl.DataFrame = customers_df.with_columns(
+        subscription_plan_id=pl.Series(
+            np.random.choice(
+                subscriptions_plans_list, size=len(customers_df), p=subscription_weights
+            )
+        )
+    )
+
+    # TODO: Finalizar a criação da tabela de snapshor de eventos
+    events: pl.DataFrame = customer_subscription.select(
+        [
+            pl.lit(None).alias("subscription_event_id"),
+            pl.col("customer_id"),
+            pl.col("subscription_plan_id"),
+            pl.lit("signup").alias("event_type"),
+            pl.col("billing_cycle"),
+            pl.lit(None).alias("price_at_event"),
+            pl.col("created_at").alias("valid_from"),
+            pl.lit(None).alias("valid_to"),
+        ]
+    )
+
+    print(events)
